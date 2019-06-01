61852da1c8c9ee633ed384e063b2f503c6ec3d88
karthick <karthiram165@gmail.com>
2019-06-01 16:36:56 +0530

    bir

---
+++ precache-manifest.c33a1d89d8781340b5ded6f001c79b73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "252b31c71091d6849041",
+    "url": "/js/chunk-vendors.d1d8582b.js"
+  },
+  {
+    "revision": "941a3ec50ffc0c437a44",
+    "url": "/js/app.5cbc7877.js"
+  },
+  {
+    "revision": "719679bec28d7dfbaf53",
+    "url": "/js/about.512aeea2.js"
+  },
+  {
+    "revision": "bd9bc7c53ac052fdd1a7ea16e10a5da4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "941a3ec50ffc0c437a44",
+    "url": "/css/app.4d05b1f6.css"
+  },
+  {
+    "revision": "719679bec28d7dfbaf53",
+    "url": "/css/about.8a2d47b8.css"
+  }
+];
